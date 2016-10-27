e6734e3e764475cc581771bc2e30c523769e2b72
Xyrephon <jtuc1613@740s-pl-115.local>
2016-10-27 08:28:10 -0600

    Wrote the overriding init for the PlayingCard class.

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -34,6 +34,39 @@ class PlayingCard : Card
         }
     }
     
+    private var color : UIColor
+    {
+        get
+        {
+            return self.color
+        }
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    
+    private var frontImage : UIImage
+    {
+        get
+        {
+            return self.frontImage
+        }
+        set(frontImage)
+        {
+            self.frontImage = frontImage
+        }
+    }
+    
+    override init()
+    {
+        super.init()
+        frontImage = UIImage()
+        color = UIColor()
+        rank = 0
+        suit = ""
+    }
+    
     func getRank() -> Int
     {
         return self.rank
