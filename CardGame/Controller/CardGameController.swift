c14f42fefc2ec222e3b8ad8f757dcf698848a98d
Xyrephon <jtuc1613@740s-pl-115.local>
2016-11-07 09:55:55 -0700

    Added a draw card method.

--- CardGame/Controller/CardGameController.swift
+++ CardGame/Controller/CardGameController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class CardGameController : UIViewController
 {
+    @IBOutlet weak var cardButton: UIButton!
+    @IBOutlet weak var cardLabel: UILabel!
+    
+    private lazy var currentDeck = PlayingCardDeck()
+    private lazy var clickCount = Int()
+    
     override func viewDidLoad()
     {
         let tempCard = Card()
         print(tempCard.toString())
         let otherTemp = PlayingCard()
         print(otherTemp.toString())
+    }
+
+    @IBAction func flipCard(sender: UIButton)
+    {
+        clickCount += 1
+        let words = "The card has been clicked \(clickCount) times"
+        cardLabel.text = words
     }
 }
