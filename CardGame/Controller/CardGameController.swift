384dbfc2d7f437ff44c34a0f6c0a1e761a91c411
Xyrephon <jtuc1613@740s-pl-115.local>
2016-11-30 09:52:45 -0700

    Built the check match for my card game.

--- CardGame/Controller/CardGameController.swift
+++ CardGame/Controller/CardGameController.swift
@@ -11,19 +11,15 @@ import UIKit
 class CardGameController : UIViewController
 {
     @IBOutlet weak var cardButton: UIButton!
-    
     @IBOutlet weak var cardLabel: UILabel!
     
+    
     private lazy var currentDeck = PlayingCardDeck()
-   
     private lazy var clickCount = Int()
     
     override func viewDidLoad()
     {
-        let tempCard = Card()
-        print(tempCard.toString())
-        let otherTemp = PlayingCard()
-        print(otherTemp.toString())
+        
     }
 
     @IBAction func flipCard(sender: UIButton)
--- CardGame/StupidGame.swift
+++ CardGame/StupidGame.swift
@@ -16,9 +16,9 @@ class StupidGame
     internal var score : Int
     internal var discardPile : Int
     internal var winnerPile : Int
-    internal var hand : Int
     internal var drawingPile : Int
     internal var drawingDeck : PlayingCardDeck
+    internal var hand : [PlayingCard]
     
     
     
@@ -28,7 +28,7 @@ class StupidGame
         self.score = 0
         self.discardPile = Int()
         self.winnerPile = Int()
-        self.hand = Int()
+        self.hand = [PlayingCard]()
         self.drawingPile = Int()
         self.drawingDeck = PlayingCardDeck()
     }
@@ -37,5 +37,60 @@ class StupidGame
     func startGame() -> Void
     {
         drawingDeck.shuffleDeck()
+        drawCards()
+    }
+    
+    private func drawCards () -> Void
+    {
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+        hand.append((drawingDeck.drawCard() as? PlayingCard)!)
+    }
+    
+    func checkMatch() -> Int
+    {
+        var points = Int()
+        
+        for (var OuterLoop = 0; OuterLoop < hand.count; OuterLoop += 1)
+        {
+            let handSize = hand.count
+            for (var InnerLoop = 0; InnerLoop < hand.count; InnerLoop += 1)
+            {
+                if(OuterLoop != InnerLoop && hand[OuterLoop].rank == hand[InnerLoop].rank)
+                {
+                    hand.removeAtIndex(InnerLoop)
+                    points += 2
+                    InnerLoop -= 1
+                }
+            }
+            
+            if (handSize != hand.count)
+            {
+                hand.removeAtIndex(OuterLoop)
+                points += 2
+                OuterLoop -= 1
+            }
+            
+        }
+        let negativePoints = hand.count
+        return points - negativePoints
+    }
+    
+    
+    func playMatchGame() -> Void
+    {
+//        while ()
+//        {
+//            
+//        }
+        checkMatch()
+        score += checkMatch()
+        hand.removeAll()
+        
+        
     }
 }
